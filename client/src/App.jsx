466ab936c1ca89809c969a6b8430e62c920ac774
amirmub <amirmubarek01@gmail.com>
2025-07-19 10:54:11 +0300

    feat: add routing and new pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
 import './App.css'
+import { Route,Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import AddEmployee from './pages/Admin/Addemployee/Addemployee'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
 
 function App() {
 
   return (
-    <h1>React</h1>
+    <>
+    <Header />
+    <Routes>
+       <Route path='/' element = {<Home />}></Route>
+       <Route path='/login' element = {<Login />}></Route>
+       <Route path='/add-employee' element = {<AddEmployee />}></Route>
+    </Routes>
+    <Footer />
+  </>
   )
 }
 
---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,50 @@
+import { Link } from 'react-router-dom';
+
+function Header(props) {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
+        <Link to="/" className="navbar-brand d-flex align-items-center px-lg-5">
+          <h2 className="m-0 text-primary">
+            <i className="fa fa-car mx-1"></i>AutoPro
+          </h2>
+        </Link>
+        <button
+          type="button"
+          className="navbar-toggler me-4"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarCollapse"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <div className="navbar-nav ms-auto p-4 p-lg-0">
+            <Link to="/" className="nav-item nav-link active">Home</Link>
+            <Link to="/about" className="nav-item nav-link">About</Link>
+            <Link to="/services" className="nav-item nav-link">Services</Link>
+            <Link to="/contact" className="nav-item nav-link">Contact</Link>
+            <div className="nav-item dropdown">
+              <Link to="/admin" className="nav-link">Admin</Link>
+            </div>
+          </div>
+          <Link
+            to="/logout"
+            className="btn btn-primary d-none d-lg-block"
+            style={{
+              width: "90px",
+              height: "45px",
+              marginRight: "30px",
+              paddingTop: "12px",
+              fontSize: "14px",
+              borderRadius: "3px",
+            }}
+          >
+            Log out
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
