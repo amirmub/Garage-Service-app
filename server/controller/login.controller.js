52dcbb1e183215287f2e83ae7c88ad15e9e30a49
amirmub <amirmubarek01@gmail.com>
2025-07-23 17:11:24 +0300

    feat: enhance login service with JWT

--- server/controller/login.controller.js
+++ server/controller/login.controller.js
@@ -6,7 +6,7 @@ async function login(req,res,next) {
     if (result.error) {
         return res.status(result.status).json({ msg : result.error})
     }
-     return res.status(result.status).json({ msg : result.message , info : result.data})
+     return res.status(result.status).json({ msg : result.message , token : result.data})
 }
 
 module.exports = { login }
--- server/services/login.service.js
+++ server/services/login.service.js
@@ -1,5 +1,6 @@
 const dbConnection = require("../config/db.config");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 async function login(loginData) {
   const { employee_email, employee_password } = loginData;
@@ -10,19 +11,23 @@ async function login(loginData) {
 
   try {
     const existedUser = await dbConnection.query(
-      `SELECT employee.employee_id, employee_pass.employee_password_hashed
-       FROM employee
-       INNER JOIN employee_pass ON employee.employee_id = employee_pass.employee_id
-       WHERE employee.employee_email = ?`,
+      `SELECT 
+        employee.employee_id, 
+        employee.employee_email, 
+        employee_pass.employee_password_hashed,
+        employee_info.employee_first_name
+      FROM employee
+      INNER JOIN employee_pass ON employee.employee_id = employee_pass.employee_id
+      INNER JOIN employee_info ON employee.employee_id = employee_info.employee_id
+      WHERE employee.employee_email = ?`,
       [employee_email]
     );
+
     console.log(existedUser);
     
     if (!existedUser || existedUser.length === 0) {
       return { error: "Account doesn't exist", status: 400 };
     }
-
-    console.log("User password:", existedUser[0].employee_password_hashed);
 
     const isMatch = await bcrypt.compare(employee_password, existedUser[0].employee_password_hashed);
 
@@ -30,10 +35,22 @@ async function login(loginData) {
       return { error: "Incorrect password", status: 400 };
     }
 
-    return { message: "Login successful", status: 200,
-      data: {
-        employee_id: existedUser[0].employee_id,
-      },
+    // Generate JWT with extended payload
+    const secretKey = process.env.JWT_SECRET;
+
+    const payload = {
+      employee_email: existedUser[0].employee_email,
+      employee_id: existedUser[0].employee_id,
+    //   company_role_id: existedUser[0].company_role_id,   // remember update this line on the SQL syntax
+      employee_first_name: existedUser[0].employee_first_name
+    };
+
+    const token = jwt.sign(payload, secretKey, { expiresIn: "1d" });
+
+    return {
+      message: "Login successful",
+      status: 200,
+      data : token
     };
   } catch (error) {
     console.error("Login error:", error);
