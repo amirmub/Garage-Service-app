f2b7a86b73d033e97d5b2b29866a8a9d2652a94f
amirmub <amirmubarek01@gmail.com>
2025-07-31 11:05:12 +0300

    implement unAuthorized page

--- client/src/pages/UnAuthorized/UnAuthorized.jsx
+++ client/src/pages/UnAuthorized/UnAuthorized.jsx
@@ -1,11 +1,28 @@
-
+import { Link } from "react-router-dom";
 
 function UnAuthorized() {
   return (
-    <div>
-      UnAuthorized
-    </div>
-  )
+    <>
+      <div className="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
+        <div className="container text-center">
+          <div className="row justify-content-center">
+            <div className="col-lg-6">
+              <i className="bi bi-exclamation-triangle display-1 text-primary"></i>
+              {/* <h3 className="display-1">403</h3> */}
+              <h1 className="mb-4">Access Denied</h1>
+              <p className="mb-4">
+                Sorry, you are not authorized to view this page. You may not
+                have permission to access this content.
+              </p>
+              <Link to="/" className="btn btn-primary rounded-pill py-3 px-5">
+                Go Back To Home
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default UnAuthorized
+export default UnAuthorized;
