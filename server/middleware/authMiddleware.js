8553d239c1681c5067861984dcef392f80e86d0a
amirmub <amirmubarek01@gmail.com>
2025-08-02 08:39:05 +0300

    feat: add authentication middleware

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+
+// to verify the token
+function tokenVerify(req, res, next) {
+  const token = req.headers.token;
+  if (!token) {
+    return res.status(400).json({ msg: "there is no token" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      console.log(err.message);
+      return res.status(400).json({ msg: "token not verified" });
+    }
+    const result = req.employee_role = decoded.employee_role;
+    console.log(result); // to check the role is sent or not // and also pass the below function
+    
+    next();
+  });
+
+}
+
+
+// next only access by admin this function to check the user is admin or not
+function isAdmin(req,res,next) {
+    // from the above function this role
+    const employee_role = req.employee_role;
+    if (employee_role === 3) {
+        next();
+    }
+    else{
+         return res.status(400).json({ msg: "You are not admin"});
+    }
+
+}
+
+module.exports = { tokenVerify,isAdmin };
--- server/routes/addEmployee.routes.js
+++ server/routes/addEmployee.routes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const addEmployeeController = require("../controller/addEmployee.controller")
+// middleware function
+const authMiddleware = require("../middleware/authMiddleware")
 
-router.post("/api/add-employee",addEmployeeController.addEmployee)
+router.post("/api/add-employee",[authMiddleware.tokenVerify,authMiddleware.isAdmin],addEmployeeController.addEmployee)
 
 module.exports = router
