f9bdecda8e5e5fb8eee5653e6ef6f5c120d1685e
amirmub <amirmubarek01@gmail.com>
2025-08-09 16:22:54 +0300

    Refactor Header component for sticky navbar

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -10,13 +10,25 @@ function Header() {
   }
 
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
+    <>
+      {/* Sticky Navbar */}
+      <nav
+        className="navbar navbar-expand-lg bg-white navbar-light shadow p-0"
+        style={{
+          position: "sticky",
+          top: 0,
+          left: 0,
+          right: 0,
+          zIndex: 1030,
+          backgroundColor: "white",
+        }}
+      >
         <Link to="/" className="navbar-brand d-flex align-items-center px-lg-5">
           <h2 className="m-0 text-primary">
             <i className="fa fa-car mx-1"></i>AutoPro
           </h2>
         </Link>
+
         <button
           type="button"
           className="navbar-toggler me-4"
@@ -25,6 +37,7 @@ function Header() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarCollapse">
           <div className="navbar-nav ms-auto p-4 p-lg-0">
             <Link to="/" className="nav-item nav-link active">
@@ -39,9 +52,6 @@ function Header() {
             <Link to="/contact" className="nav-item nav-link">
               Contact
             </Link>
-            {/* <div className="nav-item dropdown">
-              <Link to="/admin" className="nav-link">Admin</Link>
-            </div> */}
           </div>
           {isLogin ? (
             <div onClick={logOut}>
@@ -80,7 +90,9 @@ function Header() {
           )}
         </div>
       </nav>
-    </div>
+
+      {/* Spacer div to prevent content hiding behind sticky navbar */}
+    </>
   );
 }
 
