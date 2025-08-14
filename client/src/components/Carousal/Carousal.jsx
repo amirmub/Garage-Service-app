d7b76c816ffdd03e884dfad4481f3e8d63f55c13
amirmub <amirmubarek01@gmail.com>
2025-08-14 10:14:15 +0300

    refactor: Update carousel component

--- client/src/components/Carousal/Carousal.jsx
+++ client/src/components/Carousal/Carousal.jsx
@@ -1,57 +1,53 @@
+import { Carousel, Container, Row, Col } from "react-bootstrap";
 import carousel_bg_1 from "../../../assets/img/carousel-bg-1.jpg";
 import carousel_bg_2 from "../../../assets/img/carousel-bg-2.jpg";
 import carousel_1 from "../../../assets/img/carousel-1.png";
 import carousel_2 from "../../../assets/img/carousel-2.png";
 
-function Carousel() {
+function MyCarousel() {
   return (
-    <div class="container-fluid p-0 mb-5">
-        <div id="header-carousel" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img class="w-100" src={carousel_bg_1} alt="Image" />
-                    <div class="carousel-caption d-flex align-items-center">
-                        <div class="container">
-                            <div class="row align-items-center justify-content-center justify-content-lg-start">
-                                <div class="col-10 col-lg-7 text-center text-lg-start">
-                                    <h1 class="display-3 text-white mb-4 pb-3 animated slideInDown">Qualified Car Repair Service Center</h1>
-                                </div>
-                                <div class="col-lg-5 d-none d-lg-flex animated zoomIn">
-                                    <img class="img-fluid" src={carousel_1} alt="" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img class="w-100" src={carousel_bg_2} alt="Image" />
-                    <div class="carousel-caption d-flex align-items-center">
-                        <div class="container">
-                            <div class="row align-items-center justify-content-center justify-content-lg-start">
-                                <div class="col-10 col-lg-7 text-center text-lg-start">
-                                    <h1 class="display-3 text-white mb-4 pb-3 animated slideInDown">Qualified Car Wash Service Center</h1>
-                                </div>
-                                <div class="col-lg-5 d-none d-lg-flex animated zoomIn">
-                                    <img class="img-fluid" src={carousel_2} alt="" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#header-carousel"
-                data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#header-carousel"
-                data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div>
-    </div>
+    <Container fluid className="p-0 mb-5">
+      <Carousel>
+        {/* Slide 1 */}
+        <Carousel.Item>
+          <img className="d-block w-100" src={carousel_bg_1} alt="Slide 1" />
+          <Carousel.Caption className="d-flex align-items-center">
+            <Container>
+              <Row className="align-items-center justify-content-center justify-content-lg-start">
+                <Col xs={10} lg={7} className="text-center text-lg-start">
+                  <h1 className="display-3 text-white mb-4 pb-3 animated slideInDown">
+                    Qualified Car Repair Service Center
+                  </h1>
+                </Col>
+                <Col lg={5} className="d-none d-lg-flex animated zoomIn">
+                  <img className="img-fluid" src={carousel_1} alt="" />
+                </Col>
+              </Row>
+            </Container>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        {/* Slide 2 */}
+        <Carousel.Item>
+          <img className="d-block w-100" src={carousel_bg_2} alt="Slide 2" />
+          <Carousel.Caption className="d-flex align-items-center">
+            <Container>
+              <Row className="align-items-center justify-content-center justify-content-lg-start">
+                <Col xs={10} lg={7} className="text-center text-lg-start">
+                  <h1 className="display-3 text-white mb-4 pb-3 animated slideInDown">
+                    Qualified Car Wash Service Center
+                  </h1>
+                </Col>
+                <Col lg={5} className="d-none d-lg-flex animated zoomIn">
+                  <img className="img-fluid" src={carousel_2} alt="" />
+                </Col>
+              </Row>
+            </Container>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </Container>
   );
 }
 
-export default Carousel;
+export default MyCarousel;
