9830a2b26cfa3438e8e6cbffde1bf4629ee03e5a
amirmub <amirmubarek01@gmail.com>
2025-08-19 14:11:01 +0300

    update .env module

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
 const app = express();
 
 const cors = require("cors") 
@@ -15,9 +15,10 @@ app.use(sanitize.middleware)
 const routes = require("./routes/index")
 app.use(routes)
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port http://0.0.0.0:${PORT}`);
 });
+
 
 module.exports = app;
