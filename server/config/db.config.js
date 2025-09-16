7835c217292ea9557b42bb4fa6d91ca77d385009
amirmub <amirmubarek01@gmail.com>
2025-09-16 08:03:53 +0300

    fix: Correct server port log message formatting

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,14 +1,20 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 
-// Create a connection to the database
-const dbConnection = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    connectionLimit: 10, // Optional: Set a connection limit
+// // Create a connection to the database
+// const dbConnection = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     connectionLimit: 10, // Optional: Set a connection limit
+// });
+
+const dbConnection = mysql.createConnection(process.env.DATABASE_URL);
+
+dbConnection.connect(err => {
+  if (err) console.error("DB connection error:", err);
+  else console.log("Connected to Railway MySQL!");
 });
-
 
 async function query(sql,params) {
     const [row,fields] = await dbConnection.execute(sql,params)
